7ef6b2a4157d55e18508a8a3b359bd4255b08592
Gaspar Bauchet Oliveira <gaspar.bauchet-oliveira@etu.utc.fr>
2025-11-05 18:49:44 +0100

    message

--- plateau/Cite.h
+++ plateau/Cite.h
@@ -4,6 +4,17 @@
 
 class Cite {
 private:
-    Hexagone hexagone;  
-    Tuile* tuile;       
+    int chaine_habitation;
+    map carte; /*a revoir le vecteur*/
+    Tuile* tuile_temporaire;
+    Coordonnee position_temporaire; 
+
+public:
+    void afficher_cite();
+    Hexagone* recherche_hexagone(Coordonnee);
+    void maj_cite(Tuile*, Coordonnee&);
+    void set_tuile_temporaire(Tuile*);
+    void set_position_temporaire(Coordonnee&);
+    const vector<Construction&> get_voisins(Coordonnee&); /*a revoir le vecteur*/
+        
 };
