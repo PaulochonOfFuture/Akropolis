15b8f5af6d98c76842d554561b4998b1fb9776a3
Ryad <ryad.meziane@etu.utc.fr>
2025-11-23 21:46:36 +0100

    Classes Chaine_Habitations

--- plateau/ChaineHabitation.cpp
+++ plateau/ChaineHabitation.cpp
@@ -1,1 +1,28 @@
 #include "ChaineHabitation.h"
+
+// Opérateur += (ajout d'une chaine)
+Chaine_habitation& Chaine_habitation::operator+=(const Chaine_habitation& c2) {
+    for (Habitation* h : c2.habitations) {
+        habitations.push_back(h);
+        h->set_id_chaine_mere(id_chaine);
+    }
+
+    longueur += c2.longueur;
+
+    return *this;
+}
+
+// Opérateur += (ajout d'une habitation)
+Chaine_habitation& Chaine_habitation::operator+=(Habitation* h) {
+    habitations.push_back(h);
+    longueur++;
+
+    h->set_id_chaine_mere(id_chaine);
+
+    return *this;
+}
+
+    // Comparaison (relation d'ordre)
+bool Chaine_habitation::operator>(const Chaine_habitation& c2) const {
+    return longueur > c2.longueur;
+}
--- plateau/ChaineHabitation.h
+++ plateau/ChaineHabitation.h
@@ -4,12 +4,51 @@
 
 class Chaine_habitation {
 private:
-    std::vector<Habitation*> habitations; 
+    std::vector<Habitation*> habitations;
     int id_chaine;
     int longueur;
+
+    // compteur global pour générer des id uniques
+    static int next_id;
+
 public:
-    Chaine_habitation& operator+=(Chaine_habitation& c);
-    Chaine_habitation& operator+=(Habitation& h);
-    bool operator>(Chaine_habitation& c);
-    Chaine_habitation(int id, int l);
+
+    // Getters
+    const std::vector<Habitation*>& get_habitations() const {
+        return habitations;
+    }
+
+    int get_id_chaine() const {
+        return id_chaine;
+    }
+
+    int get_longueur() const {
+        return longueur;
+    }
+
+    // Constructeurs
+    Chaine_habitation(std::vector<Habitation*> habitations_, int id_chaine_, int longueur_)
+        : habitations(std::move(habitations_)),
+          id_chaine(id_chaine_),
+          longueur(longueur_)
+    {}
+
+    Chaine_habitation()
+        : habitations(),
+          id_chaine(next_id++),   // id généré automatiquement
+          longueur(0)
+    {}
+
+    // Opérateur += (ajout d'une chaine)
+    Chaine_habitation& operator+=(const Chaine_habitation& c2);
+
+    // Opérateur += (ajout d'une habitation)
+    Chaine_habitation& operator+=(Habitation* h);
+
+    // Comparaison (relation d'ordre)
+    bool operator>(const Chaine_habitation& c2);
 };
+
+// Initialisation du compteur d'id
+int Chaine_habitation::next_id = 0;
+
